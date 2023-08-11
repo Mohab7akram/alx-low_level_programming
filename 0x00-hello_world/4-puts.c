0945eb61938ad8e6efef9268ce556a830506294d
mohab7akram <mohabelnashar235@gmail.com>
2023-08-10 17:06:21 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,8 +8,8 @@
  * Return: Alawys 0 (success)
 */
 
-int mian(void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
