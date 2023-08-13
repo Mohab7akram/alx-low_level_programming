0d76480421f65278f5fbd5de63557b5b92d730b2
mohab7akram <mohabelnashar235@gmail.com>
2023-08-12 18:08:06 -0700

    done

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -19,6 +19,6 @@ int main(void)
 		putchar(digit + '0');
 		digit++;
 	}
-	printf("\n");
+	putchar("\n");
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Alawys 0 (success)
+*/
+
+int main(void)
+{
+         
+         return (0);
+}
