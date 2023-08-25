b7da38edd808cfc993d158ad28e447fb67ccaeec
mohab7akram <mohabelnashar235@gmail.com>
2023-08-25 11:58:38 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,11 @@
 #include "main.h"
-#include "stdio.h"
 /**
  * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
  *
  * Return: void
-*/
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i;
